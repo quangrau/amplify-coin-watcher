461104b38676d533eea0adadaf10b0a94d9f60b2
Alex Le <thanhquang.le@grabtaxi.com>
2019-09-27 02:50:29 +0800

    Init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { API } from "aws-amplify";
+import "./App.css";
 
 function App() {
+  // create coins variable and set to empty array
+  const [coins, updateCoins] = useState([]);
+  const limit = 20;
+  const start = 0;
+
+  // define function to all API
+  async function fetchCoins() {
+    console.log("fetching...");
+    const data = await API.get(
+      "cryptoapi",
+      `/coins?limit=${limit}&start=${start}`
+    );
+    console.log({ data });
+    updateCoins(data.coins);
+  }
+
+  // call fetchCoins function when component loads
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello from AWS Amplify</h1>
       </header>
+      <main className="App-main">
+        <section className="coins">
+          {coins.map((coin, index) => (
+            <div className="coins__row" key={index}>
+              <div>
+                {coin.name} - <code>{coin.symbol}</code>
+              </div>
+              <div>${coin.price_usd}</div>
+            </div>
+          ))}
+        </section>
+      </main>
     </div>
   );
 }
